43b2d884afdb66494203004076bee7b12bf865da
kalkidanteklay <kalkidanteklehaimanot@gmail.com>
2025-05-02 11:19:36 +0300

    updated table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,16 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-table',
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent {
+export class TableComponent implements AfterViewInit{
   @Input() columns: string[] = [];        
   @Input() columnHeaders: string[] = [];    
-  @Input() dataSource: any[] = []; 
+  @Input() set dataSource(value: any[]) {
+    this._dataSource = new MatTableDataSource(value);
+    if (this.paginator) {
+      this._dataSource.paginator = this.paginator;
+    }
+  }
+
+  _dataSource = new MatTableDataSource<any>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this._dataSource.paginator = this.paginator;
+  }
 
 }
